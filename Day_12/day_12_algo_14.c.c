c87a3f94757622a024a98dcbd2cd752db6b0d867
vishwanath5167 <108972599+vishwanath5167@users.noreply.github.com>
2022-07-14 22:30:30 -0700

    Add files via upload

---
+++ Day_12/day_12_algo_14.c.c
@@ -0,0 +1,6 @@
+/*Algorithm
+1)read 3 arrays
+2)print the 3 arrays 
+3)compare first element of array with remaining two arrays of every element
+4)if array elements are same then print common elemnts using the condition a1[i]==a2[j])&&(a2[j]==a3[k] using if and for loop
+5)print the common elements
---
+++ Day_12/day_12_algo_15.c.c
@@ -0,0 +1,8 @@
+/*Algorithm
+1)read a character 2d-array
+2)use strcpy fun to store first string in arr[0]
+3)use strcpy fun to store first string in arr[0]
+4)if there is empty string in the array then it should be replaced by string name cyient 
+5)by using strcmp function we check whether the array is empty or not 
+6)and then we will assign the cyient name in empty string
+7)print all the strings
